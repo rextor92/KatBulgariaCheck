a4535d8e62f9062067f979b05dd7eefaf458f5ae
Daniel Tsvetkov <rextor92@gmail.com>
2024-03-20 00:00:36 +0200

    Decoupling KatClient from timer triggered function by adding KatService in front

--- src/KatBulgariaCheck.API/Interfaces/IKatClient.cs
+++ src/KatBulgariaCheck.API/Interfaces/IKatClient.cs
@@ -1,13 +1,16 @@
 ﻿using FluentResults;
 using KatBulgariaCheck.Models.Kat;
-using KatBulgariaCheck.Models.Kat.Enums;
 
 namespace KatBulgariaCheck.API.Interfaces
 {
     public interface IKatClient
     {
-        Task<Result<KatResponse>> GetPersonalObligations(ObligatedIndividualSearchType obligatedIndividualSearchType);
+        Task<Result<KatResponse>> PersonalCheckByEgnAndIdCardAsync(string egn, string idCard);
 
-        Task<Result<KatResponse>> GetCompanyObligations();
+        Task<Result<KatResponse>> PersonalCheckByEgnAndDriversLicenseAsync(string egn, string driversLicense);
+
+        Task<Result<KatResponse>> PersonalCheckByEgnAndVehicleRegistrationAsync(string egn, string vehicleRegistration);
+
+        Task<Result<KatResponse>> GetCompanyObligationsAsync();
     }
 }
